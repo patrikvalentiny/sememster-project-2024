24fe56d1b83468e84659d00771f69ffb7ebba511
patrikvalentiny <patrikvalentiny@gmail.com>
2024-04-08 10:55:21 +0200

    init web api + websocket

---
+++ api/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+[ApiController]
+[Route("api/v1/status")]
+public class StatusController: Controller
+{
+    
+    [HttpGet]
+    public IActionResult GetStatus()
+    {
+        return Ok("API is running");
+    }
+}
---
+++ api/Program.cs
@@ -0,0 +1,80 @@
+using System.Reflection;
+using Fleck;
+using Serilog;
+
+var app = await StartupClass.Startup(args);
+app.Run();
+
+public static class StartupClass
+{
+    
+
+    public static Task<WebApplication> Startup(string[] args)
+    {
+        
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        Log.Logger = new LoggerConfiguration()
+            // .WriteTo.Console()
+            .CreateLogger();
+
+
+        builder.Host.UseSerilog();
+        
+
+        var app = builder.Build();
+        app.UseSerilogRequestLogging();
+        app.UseForwardedHeaders();
+        
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        
+        app.MapControllers();
+        
+        var server = new WebSocketServer("ws://0.0.0.0:8181");
+
+        server.Start(socket =>
+        {
+            socket.OnOpen = () =>
+            {
+                // userService.ConnectionPool.TryAdd(socket.ConnectionInfo.Id, socket);
+                Log.Information("Client connected: {Id}", socket.ConnectionInfo.Id);
+            };
+            socket.OnClose = () =>
+            {
+                Log.Information("Client disconnected: {Id}", socket.ConnectionInfo.Id);
+                // userService.ConnectionPool.TryRemove(socket.ConnectionInfo.Id, out _);
+                // userService.UserPool.TryRemove(socket.ConnectionInfo.Id, out _);
+                // foreach (var (roomId, sockets) in userService.RoomPool)
+                // {
+                    // sockets.Remove(socket.ConnectionInfo.Id);
+                // }
+            };
+            socket.OnMessage = async message =>
+            {
+                
+                try
+                {
+                    await socket.Send("Hello from server");
+                    // await app.InvokeClientEventHandler(services, socket, message);
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e, "Error handling message");
+                    // e.Handle(socket);
+                }
+            };
+        });
+        return Task.FromResult(app);
+    }
+}
