800d19efd98e26d4d8c5bda9ac59e5d76a082d43
patrikvalentiny <patrikvalentiny@gmail.com>
2024-04-13 20:14:15 +0200

    cleanup Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -13,16 +13,7 @@ public static class StartupClass
 {
     public static  void  Startup(string[] args)
     {
-        
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        
+        //setup logger
         var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json")
@@ -33,11 +24,19 @@ public static class StartupClass
             .ReadFrom.Configuration(configuration)
             // .WriteTo.Console()
             .CreateLogger();
-
-
-        builder.Host.UseSerilog();
-        builder.WebHost.UseUrls("http://*:5000");
         
+        
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Host.UseSerilog();
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        
+        builder.WebHost.UseUrls("http://*:5000");
 
         var app = builder.Build();
         app.UseSerilogRequestLogging();
@@ -51,43 +50,6 @@ public static class StartupClass
         }
         
         app.MapControllers();
-        app.UseWebSockets();
-        
-        
-        // var server = new WebSocketServer("ws://0.0.0.0:8181");
-        // server.Start(socket =>
-        // {
-        //     socket.OnOpen = () =>
-        //     {
-        //         // userService.ConnectionPool.TryAdd(socket.ConnectionInfo.Id, socket);
-        //         Log.Information("Client connected: {Id}", socket.ConnectionInfo.Id);
-        //     };
-        //     socket.OnClose = () =>
-        //     {
-        //         Log.Information("Client disconnected: {Id}", socket.ConnectionInfo.Id);
-        //         // userService.ConnectionPool.TryRemove(socket.ConnectionInfo.Id, out _);
-        //         // userService.UserPool.TryRemove(socket.ConnectionInfo.Id, out _);
-        //         // foreach (var (roomId, sockets) in userService.RoomPool)
-        //         // {
-        //         // sockets.Remove(socket.ConnectionInfo.Id);
-        //         // }
-        //     };
-        //     socket.OnMessage = async message =>
-        //     {
-        //         
-        //         try
-        //         {
-        //             await socket.Send("Hello from server");
-        //             // await app.InvokeClientEventHandler(services, socket, message);
-        //         }
-        //         catch (Exception e)
-        //         {
-        //             Log.Error(e, "Error handling message");
-        //             // e.Handle(socket);
-        //         }
-        //     };
-        // });
-        
         
         TcpProxyConfiguration proxyConfiguration = new TcpProxyConfiguration()
         {
@@ -111,19 +73,37 @@ public static class StartupClass
 
         using var websocketServer = new WebSocketServer("ws://0.0.0.0:8181");
         using var tcpProxy = new TcpProxyServer(proxyConfiguration);
-        // Task.Run(() => app.Run());
 
         // Initialize Fleck
-        websocketServer.Start(connection =>
+        websocketServer.Start(socket =>
         {
-            connection.OnOpen = () => Console.WriteLine("Connection on open");
-            connection.OnClose = () => Console.WriteLine("Connection on close");
-            connection.OnMessage = message => Console.WriteLine("Message: " + message);
+            socket.OnOpen = () =>
+            { 
+                Log.Debug("Client connected: {Id}", socket.ConnectionInfo.Id);
+            };
+            socket.OnClose = () =>
+            {
+                Log.Debug("Client disconnected: {Id}", socket.ConnectionInfo.Id);
+            };
+            socket.OnMessage = async message =>
+            {
+                Log.Debug("Message received: {Message}", message);
+                try
+                {
+                    await socket.Send("Hello from server");
+                    // await app.InvokeClientEventHandler(services, socket, message);
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e, "Error handling message");
+                    // e.Handle(socket);
+                }
+            };
         });
+        
         // Initialize the proxy
         tcpProxy.Start();
+        
         app.Run();
-        // return Task.FromResult(tcpProxy);
-        // Console.ReadKey();
     }
 }
