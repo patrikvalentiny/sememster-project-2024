b51a68810ca9910c858230ce5ab421b6cb5107d2
patrikvalentiny <patrikvalentiny@gmail.com>
2024-04-20 20:49:52 +0200

    fix api urls

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  restBaseUrl: 'https://climatectrl.render.com/api/v1',
-  wsBaseUrl: 'wss://climatectrl.render.com/',
+  restBaseUrl: 'https://climatectrl.onrender.com/api/v1',
+  wsBaseUrl: 'wss://climatectrl.onrender.com',
 };
