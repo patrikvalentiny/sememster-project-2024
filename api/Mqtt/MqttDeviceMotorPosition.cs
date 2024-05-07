16a2c612377b0439165f641acb11742e7decaec2
patrikvalentiny <patrikvalentiny@gmail.com>
2024-05-07 17:53:40 +0200

    add db migration script

--- api/Mqtt/MqttDeviceMotorPosition.cs
+++ api/Mqtt/MqttDeviceMotorPosition.cs
@@ -19,7 +19,6 @@ public class MqttDeviceMotorPosition(MqttClientGenerator mqttClientGenerator, We
             var m = e.ApplicationMessage;
             var message = m.ConvertPayloadToString();
             var position = JsonConvert.DeserializeObject<ServerSendsMotorDataDto>(message)!.Position;
-            // Log.Debug("Mqtt Message received: {Message}, Topic: {Topic}", message, m.Topic);
             var mac = m.Topic.Split('/')[2];
             if(webSocketStateService.MotorMacToConnectionId.TryGetValue(mac, out var connectionIdList))
             {
@@ -28,8 +27,6 @@ public class MqttDeviceMotorPosition(MqttClientGenerator mqttClientGenerator, We
                     if (webSocketStateService.Connections.TryGetValue(socketGuid, out var socket))
                         await socket.SendJson(new ServerSendsMotorDataDto { Mac = mac, Position = position });
                 }
-                // var socket = webSocketStateService.Connections[connectionId];
-                // await socket.SendJson(new ServerSendsMotorDataDto { Mac = m.Topic.Split('/')[2], Position = position });
             }
         };
     }
