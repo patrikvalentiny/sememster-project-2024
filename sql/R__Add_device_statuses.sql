f1a88f16c4109fbdd696f8a0bad1a05ff24bfdf1
patrikvalentiny <patrikvalentiny@gmail.com>
2024-05-13 12:49:42 +0200

    add real database migrations

---
+++ sql/R__Add_device_statuses.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO climate_ctrl.device_status (id, value) VALUES (1, 'online') ON CONFLICT DO NOTHING;
+INSERT INTO climate_ctrl.device_status (id, value) VALUES (2, 'offline') ON CONFLICT DO NOTHING;
+INSERT INTO climate_ctrl.device_status (id, value) VALUES (3, 'suspended') ON CONFLICT DO NOTHING;
